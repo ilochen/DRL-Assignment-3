bbd923527cbea872734af76223497729eba11f94
Ilo Chen <50260818+ilochen@users.noreply.github.com>
2025-04-23 14:28:17 +0800

    tuning weights

--- student_agent.py
+++ student_agent.py
@@ -263,6 +263,6 @@ class Agent:
 
         self.last_action = action
         self.skip_counter = 3
-        if random.random() < 0.03:
+        if random.random() < 0.02:
             action = random.randint(0, 11)
         return action
