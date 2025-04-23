de0f99881c8d34f413eec19a38780d039947113e
Ilo Chen <50260818+ilochen@users.noreply.github.com>
2025-04-23 18:12:39 +0800

    tuning weights

--- student_agent.py
+++ student_agent.py
@@ -7,90 +7,110 @@ import torch.nn as nn
 import random
 
 
-# class CNNDQN(nn.Module):
-#     def __init__(self, input_shape, num_actions):
-#         super(CNNDQN, self).__init__()
-#         self._input_shape = input_shape
-#         self._num_actions = num_actions
+class CNNDQN(nn.Module):
+    def __init__(self, input_shape, num_actions):
+        super(CNNDQN, self).__init__()
+        self._input_shape = input_shape
+        self._num_actions = num_actions
 
-#         self.features = nn.Sequential(
-#             nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
-#             nn.ReLU(),
-#             nn.Conv2d(32, 64, kernel_size=4, stride=2),
-#             nn.ReLU(),
-#             nn.Conv2d(64, 64, kernel_size=3, stride=1),
-#             nn.ReLU()
-#         )
+        self.features = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
 
-#         self.fc = nn.Sequential(
-#             nn.Linear(self.feature_size, 512),
-#             nn.ReLU(),
-#             nn.Linear(512, num_actions)
-#         )
+        self.fc = nn.Sequential(
+            nn.Linear(self.feature_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, num_actions)
+        )
 
-#     def forward(self, x):
-#         x = self.features(x).view(x.size(0), -1)
-#         return self.fc(x)
+    def forward(self, x):
+        x = self.features(x).view(x.size(0), -1)
+        return self.fc(x)
 
-#     @property
-#     def feature_size(self):
-#         with torch.no_grad():
-#             dummy_input = torch.zeros(1, *self._input_shape)
-#             return self.features(dummy_input).view(1, -1).size(1)
+    @property
+    def feature_size(self):
+        with torch.no_grad():
+            dummy_input = torch.zeros(1, *self._input_shape)
+            return self.features(dummy_input).view(1, -1).size(1)
 
 
-# class Agent:
-#     def __init__(self):
-#         self.action_space = gym.spaces.Discrete(12)
-#         self.net = CNNDQN((4, 84, 84), 12)
-#         self.net.load_state_dict(torch.load("pretrained.dat", map_location=torch.device("cpu")))
-#         self.net.eval()
+class Agent:
+    def __init__(self):
+        self.action_space = gym.spaces.Discrete(12)
+        self.net = CNNDQN((4, 84, 84), 12)
+        self.net.load_state_dict(torch.load("pretrained.dat", map_location=torch.device("cpu")))
+        self.net.eval()
 
-#         # Frame buffer = (4, 84, 84)
-#         self.buffer = np.zeros((4, 84, 84), dtype=np.float32)
-#         self.obs_buffer = deque(maxlen=2)
-#         self.skip_counter = 0
-#         self.last_action = 0
+        # Frame buffer = (4, 84, 84)
+        self.buffer = np.zeros((4, 84, 84), dtype=np.float32)
+        self.obs_buffer = deque(maxlen=2)
+        self.skip_counter = 0
+        self.last_action = None
 
-#     def preprocess_frame(self, obs):
-#         gray = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
-#         resized = cv2.resize(gray, (84, 84), interpolation=cv2.INTER_AREA)
-#         return resized.astype(np.float32)
+    def preprocess_frame(self, obs):
+        gray = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
+        resized = cv2.resize(gray, (84, 84), interpolation=cv2.INTER_AREA)
+        return resized[:, :, None].astype(np.uint8)
+    
+    
+    def update_frame_buffer(self, frame):
+        # Slide frames left, insert new one at the end
+        self.buffer[:-1] = self.buffer[1:]
+        self.buffer[-1] = frame
 
-#     def update_frame_buffer(self, frame):
-#         # Slide frames left, insert new one at the end
-#         self.buffer[:-1] = self.buffer[1:]
-#         self.buffer[-1] = frame
+    def act(self, obs):
+        # Simulate max pooling over last 2 raw frames
+        if len(self.obs_buffer) < 2 and self.last_action == None :
+            # FrameDownsample:
+            obs = self.preprocess_frame(obs)
+            
+            #image to pytorch
+            obs =np.moveaxis(obs, 2, 0)
 
-#     def act(self, obs):
-#         # Simulate max pooling over last 2 raw frames
-#         self.obs_buffer.append(obs)
-#         if len(self.obs_buffer) == 2:
-#             obs = np.maximum(self.obs_buffer[0], self.obs_buffer[1])
-#         else:
-#             obs = self.obs_buffer[0]
+            # Update frame buffer
+            self.update_frame_buffer(obs)
 
-#         # Preprocess: grayscale + resize (returns (84, 84))
-#         processed = self.preprocess_frame(obs)
+            # Normalize to float
+            obs = np.array(self.buffer).astype(np.float32) / 255.0
 
-#         # Update frame buffer
-#         self.update_frame_buffer(processed)
+            state_v = torch.tensor(np.array([obs], copy=False))
+            q_vals = self.net(state_v).data.numpy()[0]
+            action = np.argmax(q_vals)
+            self.last_action = action
+            self.skip_counter = 3
+            return action
+        elif self.skip_counter > 0:
+            self.skip_counter -= 1
+            self.obs_buffer.append(obs)
+            return self.last_action
+        else:
+            self.obs_buffer.append(obs)
+            obs = np.maximum(self.obs_buffer[-1], self.obs_buffer[-2])
 
-#         # Expand dims to (1, 4, 84, 84) and normalize
-#         input_tensor = torch.tensor([self.buffer], dtype=torch.float32) / 255.0
 
-#         # Action repeat
-#         if self.skip_counter > 0:
-#             self.skip_counter -= 1
-#             return self.last_action
+            # FrameDownsample:
+            obs = self.preprocess_frame(obs)
+            
+            #image to pytorch
+            obs =np.moveaxis(obs, 2, 0)
 
-#         with torch.no_grad():
-#             q_vals = self.net(input_tensor)[0].numpy()
+            # Update frame buffer
+            self.update_frame_buffer(obs)
 
-#         action = int(np.argmax(q_vals))
-#         self.last_action = action
-#         self.skip_counter = 2
-#         return action
+            # Normalize to float
+            obs = np.array(self.buffer).astype(np.float32) / 255.0
+
+            state_v = torch.tensor(np.array([obs], copy=False))
+            q_vals = self.net(state_v).data.numpy()[0]
+            action = np.argmax(q_vals)
+            self.last_action = action
+            self.skip_counter = 3
+            return action
 
 # class Agent:
 #     def __init__(self):
@@ -155,114 +175,114 @@ import random
 #         #     action = random.randint(0, 11)
 #         return action
 
-import cv2
-import gym
-import torch
-import numpy as np
-from collections import deque
-import torch.nn as nn
+# import cv2
+# import gym
+# import torch
+# import numpy as np
+# from collections import deque
+# import torch.nn as nn
 
 
-class CNNDQN(nn.Module):
-    def __init__(self, input_shape, num_actions):
-        super(CNNDQN, self).__init__()
-        self._input_shape = input_shape
-        self._num_actions = num_actions
+# class CNNDQN(nn.Module):
+#     def __init__(self, input_shape, num_actions):
+#         super(CNNDQN, self).__init__()
+#         self._input_shape = input_shape
+#         self._num_actions = num_actions
 
-        self.features = nn.Sequential(
-            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1),
-            nn.ReLU()
-        )
+#         self.features = nn.Sequential(
+#             nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+#             nn.ReLU(),
+#             nn.Conv2d(32, 64, kernel_size=4, stride=2),
+#             nn.ReLU(),
+#             nn.Conv2d(64, 64, kernel_size=3, stride=1),
+#             nn.ReLU()
+#         )
 
-        self.fc = nn.Sequential(
-            nn.Linear(self.feature_size, 512),
-            nn.ReLU(),
-            nn.Linear(512, num_actions)
-        )
+#         self.fc = nn.Sequential(
+#             nn.Linear(self.feature_size, 512),
+#             nn.ReLU(),
+#             nn.Linear(512, num_actions)
+#         )
 
-    def forward(self, x):
-        x = self.features(x).view(x.size(0), -1)
-        return self.fc(x)
+#     def forward(self, x):
+#         x = self.features(x).view(x.size(0), -1)
+#         return self.fc(x)
 
-    @property
-    def feature_size(self):
-        with torch.no_grad():
-            dummy_input = torch.zeros(1, *self._input_shape)
-            return self.features(dummy_input).view(1, -1).size(1)
+#     @property
+#     def feature_size(self):
+#         with torch.no_grad():
+#             dummy_input = torch.zeros(1, *self._input_shape)
+#             return self.features(dummy_input).view(1, -1).size(1)
 
 
-class Agent:
-    def __init__(self):
-        self.action_space = gym.spaces.Discrete(12)
-        self.net = CNNDQN((4, 84, 84), 12)
-        # self.net.load_state_dict(torch.load("/Users/ilo/Code/DRL_HW3/DRL-Assignment-3/super-mario-bros-dqn/recording/run1598/SuperMarioBros-1-1-v0.dat", map_location=torch.device("cpu")))
-#         
-        self.net.load_state_dict(torch.load("pretrained.dat", map_location=torch.device("cpu")))
-        self.net.eval()
+# class Agent:
+#     def __init__(self):
+#         self.action_space = gym.spaces.Discrete(12)
+#         self.net = CNNDQN((4, 84, 84), 12)
+#         # self.net.load_state_dict(torch.load("/Users/ilo/Code/DRL_HW3/DRL-Assignment-3/super-mario-bros-dqn/recording/run1598/SuperMarioBros-1-1-v0.dat", map_location=torch.device("cpu")))
+# #         
+#         self.net.load_state_dict(torch.load("pretrained.dat", map_location=torch.device("cpu")))
+#         self.net.eval()
 
-        self.obs_buffer = deque(maxlen=2)                # raw observations for max pooling
-        self.frame_buffer = np.zeros((4, 84, 84), dtype=np.uint8)  # stacked preprocessed frames
-        self.skip_counter = 0                            # for action repeat
-        self.last_action = None
-        self.initialized = False
+#         self.obs_buffer = deque(maxlen=2)                # raw observations for max pooling
+#         self.frame_buffer = np.zeros((4, 84, 84), dtype=np.uint8)  # stacked preprocessed frames
+#         self.skip_counter = 0                            # for action repeat
+#         self.last_action = None
+#         self.initialized = False
 
-    def preprocess(self, obs):
-        gray = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
-        resized = cv2.resize(gray, (84, 84), interpolation=cv2.INTER_AREA)
-        return resized.astype(np.uint8)
+#     def preprocess(self, obs):
+#         gray = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
+#         resized = cv2.resize(gray, (84, 84), interpolation=cv2.INTER_AREA)
+#         return resized.astype(np.uint8)
 
-    def act(self, obs):
-        if not self.initialized:
-            for i in range(4):
-                #initialize as zero
-                self.frame_buffer[i] = np.zeros((84, 84), dtype=np.uint8)
-            self.initialized = True
-        # else:
-        #     self.frame_buffer[:-1] = self.frame_buffer[1:]
-        #     self.frame_buffer[-1] = frame
-        # -- Step 1: Max pool incoming obs --
-        # self.obs_buffer.append(obs)
-        if len(self.obs_buffer) < 2 and self.last_action == None :
-            input_tensor = torch.tensor([self.frame_buffer], dtype=torch.float32) / 255.0
-            with torch.no_grad():
-                q_vals = self.net(input_tensor)[0].numpy()
-            action = int(np.argmax(q_vals))
+#     def act(self, obs):
+#         if not self.initialized:
+#             for i in range(4):
+#                 #initialize as zero
+#                 self.frame_buffer[i] = np.zeros((84, 84), dtype=np.uint8)
+#             self.initialized = True
+#         # else:
+#         #     self.frame_buffer[:-1] = self.frame_buffer[1:]
+#         #     self.frame_buffer[-1] = frame
+#         # -- Step 1: Max pool incoming obs --
+#         # self.obs_buffer.append(obs)
+#         if len(self.obs_buffer) < 2 and self.last_action == None :
+#             input_tensor = torch.tensor([self.frame_buffer], dtype=torch.float32) / 255.0
+#             with torch.no_grad():
+#                 q_vals = self.net(input_tensor)[0].numpy()
+#             action = int(np.argmax(q_vals))
 
-            self.last_action = action
-            self.skip_counter = 3
-            return action
+#             self.last_action = action
+#             self.skip_counter = 3
+#             return action
         
-        if self.skip_counter > 0:
-            self.skip_counter -= 1
-            self.obs_buffer.append(obs)
-            return self.last_action
+#         if self.skip_counter > 0:
+#             self.skip_counter -= 1
+#             self.obs_buffer.append(obs)
+#             return self.last_action
         
-        obs = np.maximum(self.obs_buffer[-1], self.obs_buffer[-2])
+#         obs = np.maximum(self.obs_buffer[-1], self.obs_buffer[-2])
 
-        # -- Step 2: Preprocess frame --
-        frame = self.preprocess(obs)
+#         # -- Step 2: Preprocess frame --
+#         frame = self.preprocess(obs)
 
-        # -- Step 3: Update frame buffer for stacking --
-        if not self.initialized:
-            for i in range(4):
-                self.frame_buffer[i] = frame
-            self.initialized = True
-        else:
-            self.frame_buffer[:-1] = self.frame_buffer[1:]
-            self.frame_buffer[-1] = frame
+#         # -- Step 3: Update frame buffer for stacking --
+#         if not self.initialized:
+#             for i in range(4):
+#                 self.frame_buffer[i] = frame
+#             self.initialized = True
+#         else:
+#             self.frame_buffer[:-1] = self.frame_buffer[1:]
+#             self.frame_buffer[-1] = frame
 
-        # -- Step 5: Q-value inference --
-        input_tensor = torch.tensor([self.frame_buffer], dtype=torch.float32) / 255.0
-        with torch.no_grad():
-            q_vals = self.net(input_tensor)[0].numpy()
-        action = int(np.argmax(q_vals))
+#         # -- Step 5: Q-value inference --
+#         input_tensor = torch.tensor([self.frame_buffer], dtype=torch.float32) / 255.0
+#         with torch.no_grad():
+#             q_vals = self.net(input_tensor)[0].numpy()
+#         action = int(np.argmax(q_vals))
 
-        self.last_action = action
-        self.skip_counter = 3
-        if random.random() < 0.03:
-            action = random.randint(0, 11)
-        return action
+#         self.last_action = action
+#         self.skip_counter = 3
+#         if random.random() < 0.03:
+#             action = random.randint(0, 11)
+#         return action
